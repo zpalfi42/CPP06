98ad633962d10f2619d9884c85e82a91c0d389f9
Zsolt Palfi <zpalfi@cbr7s1.42barcelona.com>
2023-01-27 13:16:01 +0100

    Finished checks

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -14,19 +14,71 @@ bool	isSpecialCase(std::string toConvert)
 	return (false);
 }
 
-bool	isSpecialCaseDouble(std::string toConvert)
+bool	isSpecialCaseFloat(std::string toConvert)
 {
-	std::string	cases[2] = {"+inf", "-inf"};
+	std::string	cases[3] = {"+inff", "-inff", "nanf"};
 	int	i = 0;
 
-	while (i < 2 && toConvert != cases[i])
+	while (i < 3 && toConvert != cases[i])
 		i++;
-	if (i < 2)
-		return (false);
-	return (true);	
+	if (i < 3)
+		return (true);
+	return (false);	
 }
 
-int to_int(char const *s)
+bool	isSpecialCaseDouble(std::string toConvert)
+{
+	std::string	cases[3] = {"+inf", "-inf", "nan"};
+	int	i = 0;
+
+	while (i < 3 && toConvert != cases[i])
+		i++;
+	if (i < 3)
+		return (true);
+	return (false);	
+}
+
+bool	isNum(char c)
+{
+	if ('0' <= c && c <= '9')
+		return (true);
+	return (false);
+}
+
+bool	isFloatOrDouble(std::string s, int mode)
+{
+	int	dot = 0;
+	int	f = 0;
+
+	if (mode == 0 && isSpecialCaseFloat(s))
+		return (true);
+	else if (mode == 1 && isSpecialCaseDouble(s))
+		return (true);
+	for (int i = 0; i < (int)s.length(); i++)
+	{
+		if (i == 0 && (s[i] == '-' || s[i] == '+'))
+			continue ;
+		else if (s.c_str()[i] == '.')
+		{
+			if (++dot > 1)
+				return (false);
+		}
+		else if (mode == 0 && s.c_str()[i] == 'f')
+		{
+			if (++f > 1 || i < (int)s.length() - 1)
+				return (false);
+		}
+		else if (!isNum(s.c_str()[i]))
+			return (false);
+	}
+	if (mode == 0 && (s.find_first_of('.') == s.find_first_of('f') - 1 || f == 0))
+		return (false);
+	else if (mode == 1 && s.find_first_of('.') == s.length() - 1)
+		return (false);
+	return (true);
+}
+
+int		to_int(char const *s)
 {
     if ( s == NULL || *s == '\0' )
        throw std::invalid_argument("null or empty string argument");
@@ -46,7 +98,48 @@ int to_int(char const *s)
          ++s;
     }
     return negate ? result : -result; //-result is positive!
-} 
+}
+
+int		tryChar(std::string toConvert)
+{
+	if (toConvert.length() == 3)
+		std::cout << "Is a CHAR" << std::endl;
+	else
+		std::cout << "Error: Wrong argument" << std::endl;
+}
+
+int		tryFloatOrDouble(std::string toConvert)
+{
+	if (toConvert.find_first_of('.') != toConvert.find_last_of('.'))
+		std::cout << "Error: Wrong argument" << std::endl;
+	else
+	{
+		if (isFloatOrDouble(toConvert, 0))
+			std::cout << "Is a FLOAT" << std::endl;
+		else if (isFloatOrDouble(toConvert, 1))
+			std::cout << "Is a DOUBLE" << std::endl;
+		else
+		{
+			std::cout << "Error: Wrong argument" << std::endl;
+			return (0);
+		}
+	}
+}
+
+int		tryInt(std::string toConvert)
+{
+	int	n;
+	try
+	{
+		n = to_int(toConvert.c_str());
+	}
+	catch(const std::exception& e)
+	{
+		std::cout << "Error: Wrong argument" << std::endl;
+		return (0);
+	}
+	std::cout << "Is a INT " << n << std::endl;
+}
 
 int	main( int argc, char **argv )
 {
@@ -59,36 +152,9 @@ int	main( int argc, char **argv )
 	}
 	std::string	toConvert = argv[1];
 	if (toConvert[0] == 39 && toConvert[toConvert.length() - 1] == 39)
-	{
-		if (toConvert.length() == 3)
-			std::cout << "Is a CHAR" << std::endl;
-		else
-			std::cout << "Error: Wrong argument" << std::endl;
-	}
+		tryChar(toConvert);
 	else if (toConvert.find_first_of('.') != std::string::npos || isSpecialCase(toConvert))
-	{
-		if (toConvert.find_first_of('.') != toConvert.find_last_of('.'))
-			std::cout << "Error: Wrong argument" << std::endl;
-		else
-		{
-			if (toConvert[toConvert.length() -1] == 'f' && isSpecialCaseDouble(toConvert))
-				std::cout << "Is a FLOAT" << std::endl;
-			else
-				std::cout << "Is a DOUBLE" << std::endl;
-		}
-	}
+		tryFloatOrDouble(toConvert);
 	else
-	{
-		int	n;
-		try
-		{
-			n = to_int(toConvert.c_str());
-		}
-		catch(const std::exception& e)
-		{
-			std::cout << "Error: Wrong argument" << std::endl;
-			return (0);
-		}
-		std::cout << "Is a INT " << n << std::endl;
-	}
+		tryInt(toConvert);
 }
